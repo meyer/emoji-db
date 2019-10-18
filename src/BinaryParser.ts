0743fe4b975047b46fecf7d97b0eb05fcf450018
meyer <github.com@meyer.fm>
2019-10-17 18:02:22 -0700

    Ditch the async iterator in favour of fs.promises

---
+++ src/BinaryParser.ts
@@ -0,0 +1,89 @@
+import fs from 'fs';
+
+export const getBinaryParser = async (filePath: string) => {
+  const fh = await fs.promises.open(filePath, 'r');
+  return new BinaryParser(fh);
+};
+
+// LONGDATETIME epoch is 1 Jan 1904 UTC
+// this gives us the offset since the unix epoch
+const longTimestampOffset = Date.UTC(1904, 0, 1).valueOf() / -1000;
+
+// const f2dot14 = (num = 0, fracBits = 14) => {
+//   const denominator = 1 << fracBits;
+//   console.log('%o / %o', num, denominator);
+//   return num / denominator;
+// };
+
+// console.log('0x7fff: got %o, expected %o', f2dot14(0x7fff), 1.999939);
+// console.log('0x7000: got %o, expected %o', f2dot14(0x7000), 1.75);
+// console.log('0x0001: got %o, expected %o', f2dot14(0x0001), 0.000061);
+// console.log('0x0000: got %o, expected %o', f2dot14(0x0000), 0);
+// console.log('0xffff: got %o, expected %o', f2dot14(0xffff), -0.000061);
+// console.log('0x8000: got %o, expected %o', f2dot14(0x8000), -2);
+
+// process.exit(1);
+
+export class BinaryParser {
+  constructor(fh: fs.promises.FileHandle) {
+    this.fh = fh;
+  }
+
+  private fh: fs.promises.FileHandle;
+  public position = 0;
+
+  private async readBytes(bytes: number): Promise<Buffer> {
+    const result = await this.fh.read(Buffer.alloc(bytes), 0, bytes, this.position);
+    this.position += bytes;
+    return result.buffer;
+  }
+
+  public async teardown() {
+    await this.fh.close();
+  }
+
+  public ascii = (bytes: number) => this.readBytes(bytes).then(buf => buf.toString('ascii'));
+
+  public int8 = () => this.readBytes(1).then(buf => buf.readInt8(0));
+  public uint8 = () => this.readBytes(1).then(buf => buf.readUInt8(0));
+
+  public uint16 = () => this.readBytes(2).then(buf => buf.readUInt16BE(0));
+  public int16 = () => this.readBytes(2).then(buf => buf.readInt16BE(0));
+
+  public uint24 = async () => {
+    const one = await this.uint16();
+    const two = await this.uint8();
+    return (one << 8) + two;
+  };
+
+  public uint32 = () => this.readBytes(4).then(buf => buf.readUInt32BE(0));
+  public int32 = () => this.readBytes(4).then(buf => buf.readInt32BE(0));
+
+  public fixed = async (bits: 8 | 16 | 32, pointOffset: number) => {
+    const numerator = await (bits === 8 ? this.int8() : bits === 16 ? this.int16() : bits === 32 ? this.int32() : null);
+    if (numerator === null) {
+      throw new Error('Unsupported bit length:' + bits);
+    }
+    const denominator = 1 << pointOffset;
+    return numerator / denominator;
+  };
+
+  public fword = this.int16;
+  public ufword = this.uint16;
+
+  public f2dot14 = () => this.fixed(16, 14);
+
+  public longdatetime = async () => {
+    const buf = await this.readBytes(8);
+    // we only read the last 32 bits
+    // TODO(meyer) update this before the year 2038
+    const last32Bits = buf.readUInt32BE(4);
+    const unixTimestamp = (last32Bits - longTimestampOffset) * 1000;
+    return new Date(unixTimestamp);
+  };
+
+  public tag = () => this.ascii(4);
+
+  public offset16 = this.uint16;
+  public offset32 = this.uint32;
+}
--- src/cli.ts
+++ src/cli.ts
@@ -1,22 +1,41 @@
 // https://docs.microsoft.com/en-us/typography/opentype/spec/otff
 import path from 'path';
-import fs from 'fs';
-import { WrappedAsyncIterator } from './WrappedAsyncIterator';
+import { getBinaryParser, BinaryParser } from './BinaryParser';
 
 const ROOT_DIR = path.resolve(__dirname, '..');
 const FONTS_DIR = path.join(ROOT_DIR, 'fonts');
 
+process.on('uncaughtException', err => {
+  console.error('Uncaught exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', err => {
+  console.error('Uncaught rejection:', err);
+  process.exit(1);
+});
+
+/**
+ * Print a number as a zero-padded hex string with pad lengths equal to powers of 2.
+ * Minimum pad length is 4.
+ */
+const numToHex = (num: number) => {
+  const numStr = num.toString(16);
+  const padLen = numStr.length <= 4 ? 4 : Math.pow(2, Math.ceil(Math.log2(numStr.length)));
+  return `0x${numStr.padStart(padLen, '0')}`;
+};
+
 (async argv => {
-  let fd: number | undefined;
+  let bp: BinaryParser | undefined;
   try {
     if (argv.length !== 1) {
       throw new Error('one arg pls');
     }
 
     const fontPath = path.join(FONTS_DIR, argv[0]);
-    const buf = new WrappedAsyncIterator(fontPath);
+    bp = await getBinaryParser(fontPath);
 
-    const header = await buf.ascii();
+    const header = await bp.ascii(4);
 
     const isTtcf = header === 'ttcf';
 
@@ -26,33 +45,65 @@ const FONTS_DIR = path.join(ROOT_DIR, 'fonts');
       console.log('we have a TTCF');
     }
 
-    const majorVersion = await buf.uint16();
+    const majorVersion = await bp.uint16();
+    const minorVersion = await bp.uint16();
 
-    if (majorVersion !== 2) {
+    const version = (majorVersion << 16) + minorVersion;
+
+    if (version !== 0x00020000) {
       throw new Error('Only TTC version 2 is supported for now');
     }
 
-    const minorVersion = await buf.uint16();
-    const numFonts = await buf.uint32();
-    const offsetTable = await buf.uint32();
-    const dsigTag = await buf.uint32();
-    const dsigLength = await buf.uint32();
-    const dsigOffset = await buf.uint32();
+    const numFonts = await bp.uint32();
+
+    const offsets: number[] = [];
+
+    for (let i = 0; i < numFonts; i++) {
+      offsets[i] = await bp.uint32();
+    }
+
+    const dsigTag = await bp.uint32();
+    const dsigLength = await bp.uint32();
+    const dsigOffset = await bp.uint32();
 
     console.log({
       majorVersion,
       minorVersion,
+      version: numToHex(version),
       numFonts,
-      offsetTable,
+      offsets,
       dsigTag,
       dsigLength,
       dsigOffset,
     });
+
+    for (const offset of offsets) {
+      bp.position = offset;
+
+      const sfntVersion = await bp.uint32();
+      const numTables = await bp.uint16();
+      const searchRange = await bp.uint16();
+      const entrySelector = await bp.uint16();
+      const rangeShift = await bp.uint16();
+
+      if (sfntVersion !== 0x00010000) {
+        throw new Error('sfntVersion !== 0x00010000 (got ' + sfntVersion + ')');
+      }
+
+      console.log({
+        offset,
+        sfntVersion,
+        numTables,
+        searchRange,
+        entrySelector,
+        rangeShift,
+      });
+    }
   } catch (err) {
     console.error(err);
   } finally {
-    if (fd != null) {
-      fs.closeSync(fd);
+    if (bp) {
+      await bp.teardown();
     }
   }
 })(process.argv.slice(2));
