ceb01b6afc0d3a536d159035215277b9d3bc4838
meyer <github.com@meyer.fm>
2019-12-26 17:29:44 -0800

    Add convert-emojilib-data script

---
+++ src/scripts/convertEmojilibData.ts
@@ -0,0 +1,34 @@
+// https://docs.microsoft.com/en-us/typography/opentype/spec/otff
+import path from 'path';
+import fs from 'fs';
+import { DATA_DIR } from '../constants';
+import { invariant } from '../utils/invariant';
+import stringify from 'json-stable-stringify';
+import { sortKeyStringifyOptions } from '../utils/sortKeyStringifyOptions';
+import { toEmojiSortKey } from '../utils/toEmojiSortKey';
+import emojilibData from 'emojilib/emojis.json';
+import { toCodepoints } from '../utils/toCodepoints';
+import { toEmojiKey } from '../utils/toEmojiKey';
+
+(async () => {
+  const emojilibDataByEmojiKey: Record<string, any> = {};
+
+  for (const [key, value] of Object.entries(emojilibData)) {
+    const codepoints = toCodepoints(value.char);
+    const emojiKey = toEmojiKey(codepoints);
+    const sortKey = toEmojiSortKey(codepoints);
+
+    invariant(!emojilibDataByEmojiKey.hasOwnProperty(emojiKey), 'Duplicate emoji key:', emojiKey);
+
+    emojilibDataByEmojiKey[emojiKey] = {
+      emojilibKey: key,
+      sortKey,
+      ...value,
+    };
+  }
+
+  fs.writeFileSync(path.join(DATA_DIR, 'emojilib.json'), stringify(emojilibDataByEmojiKey, sortKeyStringifyOptions));
+})().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
