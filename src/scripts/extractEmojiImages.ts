8fcd08f8b354b613a5ba2cf9bc600238ddb423ed
meyer <github.com@meyer.fm>
2021-05-08 01:03:39 -0400

    Fix error muffling in extractEmojiImage script

--- src/scripts/extractEmojiImages.ts
+++ src/scripts/extractEmojiImages.ts
@@ -11,23 +11,32 @@ import { getMetadataForEmojiKey } from '../utils/getMetadataForEmojiKey';
   invariant(argv.length === 1, 'one arg pls');
   const fontPath = path.join(FONTS_DIR, argv[0]);
   const ttf = await getFontByName(fontPath, 'AppleColorEmoji');
+  const errors = [];
 
   try {
     await fs.promises.mkdir(EMOJI_IMG_DIR);
     for await (const { data, name } of ttf.getEmojiIterator()) {
       const key = emojiNameToKey(name);
-      const { fileName } = getMetadataForEmojiKey(key);
-      const absPath = path.join(EMOJI_IMG_DIR, fileName + '.png');
+      try {
+        const { fileName } = getMetadataForEmojiKey(key);
+        const absPath = path.join(EMOJI_IMG_DIR, fileName + '.png');
 
-      await fs.promises.writeFile(absPath, data, {
-        // write should fail if the file already exists
-        flag: 'wx',
-      });
+        await fs.promises.writeFile(absPath, data, {
+          // write should fail if the file already exists
+          flag: 'wx',
+        });
+      } catch (err) {
+        errors.push(err);
+        console.error('Error with %s (%s):', key, name, err);
+      }
     }
   } catch (err) {
-    console.error(err);
-  } finally {
     await ttf.fh.close();
+    throw err;
+  }
+
+  if (errors.length) {
+    throw new Error('Error extracting images');
   }
 })(process.argv.slice(2)).catch((err) => {
   console.error(err);
