fffd3f660a3ffa75e92418867fbcee8453fdcc65
meyer <github.com@meyer.fm>
2024-11-11 16:26:29 +0200

    Update Unicode data URLs

--- scripts/fetchData.sh
+++ scripts/fetchData.sh
@@ -2,11 +2,13 @@
 
 rm -rf cache
 mkdir -p cache
-wget -O cache/annotations.xml https://raw.githubusercontent.com/unicode-org/cldr/master/common/annotations/en.xml
+
+wget -O cache/annotations.xml        https://raw.githubusercontent.com/unicode-org/cldr/master/common/annotations/en.xml
 wget -O cache/annotationsDerived.xml https://raw.githubusercontent.com/unicode-org/cldr/master/common/annotationsDerived/en.xml
-wget -O cache/emoji-data.txt https://unicode.org/Public/13.0.0/ucd/emoji/emoji-data.txt
-wget -O cache/emoji-sequences.txt https://unicode.org/Public/emoji/latest/emoji-sequences.txt
-wget -O cache/emoji-test.txt https://unicode.org/Public/emoji/latest/emoji-test.txt
-wget -O cache/emoji-variation-sequences.txt https://unicode.org/Public/13.0.0/ucd/emoji/emoji-variation-sequences.txt
+wget -O cache/emoji-sequences.txt     https://unicode.org/Public/emoji/latest/emoji-sequences.txt
 wget -O cache/emoji-zwj-sequences.txt https://unicode.org/Public/emoji/latest/emoji-zwj-sequences.txt
-wget -O cache/emoji-ordering.txt https://www.unicode.org/emoji/charts-12.1/emoji-ordering.txt
+wget -O cache/emoji-test.txt     https://unicode.org/Public/emoji/latest/emoji-test.txt
+wget -O cache/emoji-ordering.txt https://unicode.org/emoji/charts/emoji-ordering.txt
+
+# TODO: updated path for this file, or a replacement
+wget -O cache/emoji-variation-sequences.txt https://unicode.org/Public/13.0.0/ucd/emoji/emoji-variation-sequences.txt
